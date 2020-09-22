83f68ebc9ed80ad616a32e13cc6e4bd5c453f0c0
Matt Javier <matthewrjavier@gmail.com>
2020-09-22 13:32:27 -0700

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,45 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles' 
+
+import About from './pages/About'
+import LogIn from './pages/LogIn'
+import Matches from './pages/Matches'
+import Profile from './pages/Profile'
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: '#263238'
+    },
+    primary: {
+      main: '#263238',
+    },
+    secondary: {
+      main: '#845bb3',
+    },
+  },
+})
+
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <CssBaseline />
+          
+          <Switch>
+            <Route exact path="/" component={LogIn}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/profile" component={Profile}/>
+            <Route exact path="/match" component={Matches}/>
+          </Switch>
+
+        </div>
+      </MuiThemeProvider>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Matches/Matches.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Matches = () => {
+  return (
+    <h1>Matches</h1>
+  )
+}
+
+export default Matches
