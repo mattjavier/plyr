22ca6040ea6e6cb69929d9a8e55e583bb15a089b
Matt Javier <matthewrjavier@gmail.com>
2020-09-22 14:34:18 -0700

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import About from './pages/About'
-import LogIn from './pages/LogIn'
-import Matches from './pages/Matches'
-import Profile from './pages/Profile'
-import logo from './assets/logo.jpg'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles' 
+import logo from './assets/logo.jpg'
+
+import Footer from './components/Footer'
 
 import About from './pages/About'
 import LogIn from './pages/LogIn'
@@ -20,10 +18,14 @@ const theme = createMuiTheme({
       default: '#263238'
     },
     primary: {
-      main: '#263238',
+      main: '#845bb3',
     },
     secondary: {
       main: '#845bb3',
+    },
+    text: {
+      primary: '#845bb3',
+      secondary: '#f3e5f5'
     },
   },
 })
@@ -34,14 +36,14 @@ const App = () => {
       <MuiThemeProvider theme={theme}>
         <div>
           <CssBaseline />
-          
+          {/* <Navbar /> */}
           <Switch>
             <Route exact path="/" component={LogIn}/>
             <Route exact path="/about" component={About}/>
             <Route exact path="/profile" component={Profile}/>
             <Route exact path="/match" component={Matches}/>
           </Switch>
-
+          <Footer />
         </div>
       </MuiThemeProvider>
     </Router>
--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  label: {
+    color: '#f3e5f5'
+  }
+}))
+
 const LogIn = () => {
+  const classes = useStyles()
+
   return (
-    <h1>Log In</h1>
+    <form>
+      <p className={classes.label}>Username</p>
+    </form>
   )
 }
 
