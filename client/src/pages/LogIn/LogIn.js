cf355db05da93dd3ae6a59b314a5d1629f921fbd
Pat Downer <patricia.downer@gmail.com>
2020-09-23 12:12:02 -0700

    better format for password field, added buttons

--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import Button from '@material-ui/core/Button'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import IconButton from '@material-ui/core/IconButton'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Register from '../../components/Register'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,24 +20,79 @@ const useStyles = makeStyles((theme) => ({
       width: '25ch',
     },
   },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '25ch',
+  },
 }));
 
 
 const LogIn = () => {
   const classes = useStyles()
 
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+
   return (
     <>
-    <h1>Log In</h1>
+      <h1>Log In</h1>
       <form className={classes.root} noValidate autoComplete="off">
         <TextField
           required
           id="outlined-required"
           label="Username"
-          defaultValue="Username"
           variant="outlined"
         />
-        </form>
+        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            required
+            id="outlined-adornment-password"
+            label="Password"
+            variant="outlined"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button>Log In</Button>
+        <Register />
+
+      </form>
     </>
   )
 }
