3eb56162e05a39c2d36494dcc5b5b4db37dcb991
Pat Downer <patricia.downer@gmail.com>
2020-09-23 13:12:53 -0700

    added input fields for register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import IconButton from '@material-ui/core/IconButton'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -27,6 +36,21 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '25ch',
+  },
 }));
 
 const Register = () => {
@@ -43,13 +67,82 @@ const Register = () => {
     setOpen(false);
   };
 
+  const handleSubmit = () => {
+    setOpen(false);
+    // post request goes here
+  };
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   const body = (
     <div style={modalStyle} className={classes.paper}>
       <h2 id="simple-modal-title">Register</h2>
       <p id="simple-modal-description">
-        forms are going here
+        <form className={classes.root} noValidate autoComplete="off">
+          {/* Email Field */}
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            type="email"
+            variant="outlined"
+          />
+
+          {/* Username Field */}
+          <TextField
+            required
+            id="outlined-required"
+            label="Username"
+            variant="outlined"
+          />
+
+          {/* Password field */}
+          <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              required
+              id="outlined-adornment-password"
+              label="Password"
+              variant="outlined"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+
+          <Button onClick={handleSubmit}>Submit</Button>
+        </form>
       </p>
-      <Button>Submit</Button>
     </div>
   );
 
