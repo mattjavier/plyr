68ecfb2223d785f90eecc7cdb7711cdc44c44334
Pat Downer <patricia.downer@gmail.com>
2020-09-23 11:06:23 -0700

    nav bar functional but ugly

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,25 @@ import LogIn from './pages/LogIn'
 import Matches from './pages/Matches'
 import Profile from './pages/Profile'
 import logo from './assets/logo.jpg'
+import Navbar from './components/Navbar'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles' 
-
-import About from './pages/About'
-import LogIn from './pages/LogIn'
-import Matches from './pages/Matches'
-import Profile from './pages/Profile'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
     background: {
       default: '#263238'
     },
+    surface: {
+      default: '#4f5b62'
+    },
     primary: {
-      main: '#263238',
+      main: '#845bb3',
     },
     secondary: {
-      main: '#845bb3',
+      main: '#414679',
     },
   },
 })
@@ -34,12 +33,12 @@ const App = () => {
       <MuiThemeProvider theme={theme}>
         <div>
           <CssBaseline />
-          
+          <Navbar />
           <Switch>
-            <Route exact path="/" component={LogIn}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/profile" component={Profile}/>
-            <Route exact path="/match" component={Matches}/>
+            <Route exact path="/" component={LogIn} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/matches" component={Matches} />
           </Switch>
 
         </div>
