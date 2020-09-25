dcda4363af54e8a15d457efc9478664dfb0bc604
Pat Downer <patricia.downer@gmail.com>
2020-09-25 14:04:17 -0700

    removing build steps b/c we're not using  stepper

--- client/src/components/BuildProfile/BuildProfile.js
+++ client/src/components/BuildProfile/BuildProfile.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import FormControl from '@material-ui/core/FormControl'
-import Build_Step1 from '../Build_Step1'
-import Build_Step2 from '../Build_Step2'
-import Build_Step3 from '../Build_Step3'
+import TextField from '@material-ui/core/TextField'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import IconButton from '@material-ui/core/IconButton'
+import InputLabel from '@material-ui/core/InputLabel'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Switch from '@material-ui/core/Switch'
+import System from '../System'
+import Genre from '../Genre'
+import Game from '../Game'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,20 +22,94 @@ const useStyles = makeStyles((theme) => ({
       width: '75%',
     },
   },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '25ch',
+  },
 }));
 
 const BuildProfile = () => {
 
   const classes = useStyles()
 
+  const [state, setState] = React.useState({
+    checkedA: false
+  });
+
+  const handleSwitchChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const [profile, setBuild_Step1] = React.useState({
+    username: '',
+    bio: '',
+    competitive: false,
+    systems: [],
+    games: [],
+    genres: [],
+    profiles: []
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <form className={classes.root} noValidate autoComplete="off">
 
-      <Build_Step1 />
+      {/* Bio */}
+      <p>
+        <TextField
+          id="outlined-multiline-static"
+          label="Bio"
+          placeholder="Describe your gaming style."
+          multiline
+          rows={4}
+          variant="outlined"
+        />
+      </p>
 
-      <Build_Step2 />
+      {/* Discord username */}
+      <TextField
+        id="outlined-required"
+        label="Discord Username"
+        variant="outlined"
+      />
 
-      <Build_Step3 />
+      {/* Competitive Switch */}
+      <p>Which best describes your playing style?</p>
+      <p>Casual <Switch
+        checked={state.checkedA}
+        onChange={handleSwitchChange}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      /> Competitive</p>
+
+
+      <System />
+      <Genre />
+      <Game />
 
       <p>
         <Button>Save</Button>
