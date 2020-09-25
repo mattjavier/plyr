8da9272a8488044b0f56717fc4f9c6086313ae7d
Mak <makotorasahi@gmail.com>
2020-09-25 14:25:26 -0700

    image post works from front-end

--- client/src/App1.js
+++ client/src/App1.js
@@ -4,9 +4,33 @@ import { Base64 } from 'js-base64'
 
 
 const App = () => {
+
     const [imageState, setImageState] = useState({
         images: []
     })
+
+    imageState.handleInputChange = event => {
+        setImageState({ ...imageState, [event.target.name]: event.target.value })
+    }
+
+    imageState.handleUpload = event => {
+        setImageState({ ...imageState, image: event.target.files[0]})
+    }
+
+    imageState.handleUploadImage = event => {
+        event.preventDefault()
+        console.log('will upload')
+        const formData = new FormData()
+        formData.append("name", imageState.name)
+        formData.append("description", imageState.description)
+        formData.append("image", imageState.image)
+        console.log(formData)
+        axios.post('/api/images', formData)
+            .then(image => {
+                console.log(image)
+            })
+            .catch(err => console.log(err))
+    }
 
     useEffect(() => {
         axios.get('/api/images')
@@ -19,6 +43,7 @@ const App = () => {
         .catch(err => console.log(err))
     }, []) 
 
+
     return (
         <>
             <h1>Photo testing</h1>
@@ -27,6 +52,26 @@ const App = () => {
             return(<img src={`data:${image.contentType};base64,` + `${image.data}`} />)
             })}
 
+            <hr />
+
+            <h1>Image uploading</h1>
+            <form encType="multipart/form-data">
+                <p>
+                    <label>File Name: </label>
+                    <input type="text" name="name" onChange={imageState.handleInputChange} />
+                </p>
+                <p>
+                    <label>File Description: </label>
+                    <input type="text" name="description" onChange={imageState.handleInputChange} />
+                </p>
+                <p>
+                    <label>File: </label>
+                    <input type="file" name="image" onChange={imageState.handleUpload} />
+                </p>
+                <p>
+                    <button onClick={imageState.handleUploadImage}>Upload</button>
+                </p>
+            </form> 
 
         </>
     )
