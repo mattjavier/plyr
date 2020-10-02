e0625c4a7a72c26e339d6da29c5b37dc9b4fb362
Pat Downer <patricia.downer@gmail.com>
2020-10-01 18:44:23 -0700

    draft cards for users

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -4,26 +4,40 @@ import Developer from '../../components/Developer'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+// avatars
+import pokeball015 from '../../assets/userIcons/png/015-pokeball.png'
+import spiderman022 from '../../assets/userIcons/png/022-spiderman.png'
+import batman024 from '../../assets/userIcons/png/024-batman.png'
+import helmet019 from '../../assets/userIcons/png/019-helmet.png'
+
 const developers = [
   {
     name: "Matt Javier",
-    role: "gitmaster, front end, and back end",
-    link: "https://github.com/mattjavier"
+    title: "gitmaster, front end, and back end",
+    details: 'Fill in the main things you worked on for this project.',
+    link: "https://github.com/mattjavier",
+    avatar: helmet019,
   },
   {
     name: "Pat Downer",
-    role: "front end",
-    link: "https://github.com/PatDowner"
+    title: "front end",
+    details: 'Fill in the main things you worked on for this project.',
+    link: "https://github.com/PatDowner",
+    avatar: pokeball015,
   },
   {
     name: "Makoto Asahi",
-    role: "back end",
-    link: "https://github.com/mrasahi"
+    title: "back end",
+    details: 'Fill in the main things you worked on for this project.',
+    link: "https://github.com/mrasahi",
+    avatar: batman024,
   },
   {
     name: "Eric Park",
-    role: "back end",
-    link: "https://github.com/E-Park8"
+    title: "back end",
+    details: 'Fill in the main things you worked on for this project.',
+    link: "https://github.com/E-Park8",
+    avatar: spiderman022,
   }
 ]
 
@@ -49,7 +63,7 @@ const About = () => {
       <Typography color="textPrimary" variant="h5" component="h5">
         This app was built by a team of 4 developers as the final project for the U.C. Irvine Coding Boot Camp.
       </Typography>
-      <Grid container spacing={3}>
+      <Grid container spacing={1}>
         {developers.map(developer => (
           <Developer developer={developer} />))}
       </Grid>
