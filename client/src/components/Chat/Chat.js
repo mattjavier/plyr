a21614d1ae09b511a9583e35fa49fe1c31c3e7ed
Matt Javier <matthewrjavier@gmail.com>
2020-10-04 01:39:42 -0700

    one last test

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ import Box from '@material-ui/core/Box'
 
 
 // Connects to server 3002 where socket is run
-const socket = io.connect('https://plyr-2.herokuapp.com/socket.io/socket.io.js')
+const socket = io.connect(process.env.NODE_ENV || `http://localhost:3002`)
 
 let height = window.innerHeight - 360
 
