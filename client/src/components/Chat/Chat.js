928ae69e65b8ac2958fcaf539a2164387c9011c4
Matt Javier <matthewrjavier@gmail.com>
2020-10-04 03:22:06 -0700

    test deploy

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ import Box from '@material-ui/core/Box'
 
 
 // Connects to server 3002 where socket is run
-const socket = io.connect()
+const socket = io.connect('https://plyr-2.herokuapp.com/globalchat:3002', { secure: true, reconnection: true, rejectUnauthorized: false })
 // const socket = io.connect('https://plyr-2.herokuapp.com/globalchat:3002' || 'http://localhost:3002')
 
 
