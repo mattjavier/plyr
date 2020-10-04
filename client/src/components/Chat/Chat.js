fc3a03908b4bd69b3a55bb40260d311e4933d9bd
Matt Javier <matthewrjavier@gmail.com>
2020-10-04 01:49:50 -0700

    test once more

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ import Box from '@material-ui/core/Box'
 
 // Connects to server 3002 where socket is run
 //const socket = io.connect(process.env.NODE_ENV || `http://localhost:3002`)
-const socket = io('https://plyr-2.herokuapp.com/', {transports: ['websocket']})
+const socket = io('https://plyr-2.herokuapp.com/globalchat', {transports: ['websocket']})
 
 let height = window.innerHeight - 360
 
