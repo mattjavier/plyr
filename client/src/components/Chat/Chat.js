c3e24419f3808b5faf212aeffd2340cf098efad2
Matt Javier <matthewrjavier@gmail.com>
2020-10-08 11:32:03 -0700

    deployment changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ import Box from '@material-ui/core/Box'
 
 
 // Connects to server 3002 where socket is run
-const socket = io.connect(process.env.PORT || `http://localhost:3002`)
+const socket = io(`ws://plyr-2.herokuapp.com/globalchat:${process.env.PORT}` || `http://localhost:3002`)
 // const socket = io.connect('https://plyr-2.herokuapp.com/globalchat:3002' || 'http://localhost:3002')
 
 // test
